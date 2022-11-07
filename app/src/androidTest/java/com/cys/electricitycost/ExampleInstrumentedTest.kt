17ffb261619783bd9dd05a07e4328ddec186eb42
DevCrazz <saracorraless06@gmail.com>
2022-11-07 10:37:10 +0100

    prepare retrofit

--- app/src/androidTest/java/com/cys/electricitycost/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cys/electricitycost/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.cys.electricitycost", appContext.packageName)
+        assertEquals("com.cys.myjetpackcompose", appContext.packageName)
     }
 }
